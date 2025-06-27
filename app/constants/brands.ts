786483471a16302b92ba56715eba409026b0ba89
Jesus Udiz <138803352+udizJg@users.noreply.github.com>
2025-06-27 14:06:59 -0400

    Update brands.ts

--- app/constants/brands.ts
+++ app/constants/brands.ts
@@ -6,7 +6,7 @@ export const PARENT = {
   1501686: env.get('COUNTRY_CODE') === 'CL' ? 0 : env.get('COUNTRY_CODE') === 'CO' ? 3057 :  2674, // AR
   1461778: 0, // TS
   1573014: 0,  // SF
-  1598942: env.get('COUNTRY_CODE') === 'CL' ? 1828 : env.get('COUNTRY_CODE') === 'CO' ? 2423 : 
+  1598942: env.get('COUNTRY_CODE') === 'CL' ? 1828 : env.get('COUNTRY_CODE') === 'CO' ? 2423 : 2203,// UC
   1420393: env.get('COUNTRY_CODE') === 'CL' ? 1338  : 1470 //  <----FC PE
   1567036: env.get('COUNTRY_CODE') === 'CL' ? 1342 : 0, // C.C
 } as const;
