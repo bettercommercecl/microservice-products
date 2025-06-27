e31b0d33802b9b2e810559785812859cadabe2b1
Jesus Udiz <138803352+udizJg@users.noreply.github.com>
2025-06-27 14:05:23 -0400

    Update brands.ts

--- app/constants/brands.ts
+++ app/constants/brands.ts
@@ -1,12 +1,14 @@
+import env from '#start/env'
 export const PARENT = {
-  1: 4159,
-  1443267: 0,
-  1457601: 4158,
-  1501686: 0,
-  1461778: 0,
-  1573014: 0,
-  1598942: 1828,
-  1420393: 1338
+  1: env.get('COUNTRY_CODE') ==='CL' ? 4159 : env.get('COUNTRY_CODE') === 'CO' ? 3056 :  2780, // UF
+  1443267: env.get('COUNTRY_CODE') ==='CL' ? 0 : env.get('COUNTRY_CODE') === 'CO' ? 2933 : 1838, // AF
+  1457601: env.get('COUNTRY_CODE') ==='CL' ? 4158 : env.get('COUNTRY_CODE') === 'CO' ? 2931 : 2717, //TF
+  1501686: env.get('COUNTRY_CODE') === 'CL' ? 0 : env.get('COUNTRY_CODE') === 'CO' ? 3057 :  2674, // AR
+  1461778: 0, // TS
+  1573014: 0,  // SF
+  1598942: env.get('COUNTRY_CODE') === 'CL' ? 1828 : env.get('COUNTRY_CODE') === 'CO' ? 2423 : 
+  1420393: env.get('COUNTRY_CODE') === 'CL' ? 1338  : 1470 //  <----FC PE
+  1567036: env.get('COUNTRY_CODE') === 'CL' ? 1342 : 0,
 } as const;
   
 
