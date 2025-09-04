b67dc4aa7178692ed8cf4de0c495701ddf16e99a
Jesús Udiz <jesus.udiz@bettercommerce.cl>
2025-09-04 10:24:56 -0400

    feat: agregar interfaz SafeStockItem, definiendo la estructura de datos para la gestión de inventario, mejorando la representación y manejo de artículos en stock

---
+++ app/interfaces/inventory_interface.ts
@@ -0,0 +1,13 @@
+export interface SafeStockItem {
+  identity: {
+    sku: string
+    variant_id: number
+    product_id: number
+  }
+  settings: {
+    safety_stock: number
+    warning_level: number
+    bin_picking_number: string
+  }
+  available_to_sell: number
+}
