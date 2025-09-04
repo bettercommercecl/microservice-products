03bf4d7621a12d11baf10de89d7d7721a766e9f9
Jesús Udiz <jesus.udiz@bettercommerce.cl>
2025-09-04 10:27:23 -0400

    feat: mejorar PriceService con registro de logs para la obtención de precios, incluyendo mensajes informativos y de error para una mejor trazabilidad

--- app/services/price_service.ts
+++ app/services/price_service.ts
@@ -1,9 +1,13 @@
 import env from '#start/env'
 import axios from 'axios'
+import Logger from '@adonisjs/core/services/logger'
 
 class PriceService {
-  static async getPriceByVariantId(variant_id: number) {
+  async getPriceByVariantId(variant_id: number) {
+    const logger = Logger.child({ service: 'PriceService' })
     try {
+      logger.info(`💰 Obteniendo precio para variante ${variant_id}...`)
+
       const listPriceId = env.get(`LIST_PRICE_ID_${env.get('COUNTRY_CODE')}`)
       const url = `${env.get('URL_MICROSERVICE_PRICES')}/price/${variant_id}/${listPriceId}`
       const response = await axios.get(url, {
@@ -12,9 +16,11 @@ class PriceService {
         },
         timeout: 15000,
       })
+
+      logger.info(`✅ Precio obtenido para variante ${variant_id}: $${response.data?.price || 0}`)
       return response.data
     } catch (error) {
-      console.error(error)
+      logger.error(`❌ Error al obtener precio para variante ${variant_id}:`, error)
       throw error
     }
   }
