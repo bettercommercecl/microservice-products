523e990f6bcacaab9e655fda6c49238a50e695b0
Jesús Udiz <jesus.udiz@bettercommerce.cl>
2025-09-04 10:24:39 -0400

    feat: agregar interfaz ChannelConfigInterface para la configuración de canales, definiendo propiedades clave como URL de API, moneda y tamaños por tienda, mejorando la estructura del proyecto

---
+++ app/interfaces/channel_interface.ts
@@ -0,0 +1,62 @@
+export interface ChannelConfigInterface {
+  CHANNEL: number
+  API_URL: string
+  CLIENT_URL: string
+  CURRENCY: 'CLP' | 'COP' | 'PEN'
+  PARENT_CATEGORY: number | null
+  ID_SAMEDAY: number | null
+  ID_24HORAS: number | null
+  ID_PICKUP_IN_STORE: number | null
+  ID_FREE_SHIPPING: number | null
+  ID_TURBO: number | null
+  ID_BENEFITS: number | null
+  ID_CAMPAIGNS: number | null
+  PERCENT_DISCOUNT_TRANSFER_PRICE: number | null
+  ID_RESERVE: number | null
+  // Variables de tamaños por tienda
+  ID_SMALL_NAPOLEON: number | null
+  ID_MEDIUM_NAPOLEON: number | null
+  ID_BIG_NAPOLEON: number | null
+  ID_SMALL_VITACURA: number | null
+  ID_MEDIUM_VITACURA: number | null
+  ID_BIG_VITACURA: number | null
+  ID_SMALL_CONDOR: number | null
+  ID_MEDIUM_CONDOR: number | null
+  ID_BIG_CONDOR: number | null
+  ID_SMALL_QUILICURA: number | null
+  ID_MEDIUM_QUILICURA: number | null
+  ID_BIG_QUILICURA: number | null
+  ID_SMALL_VINA: number | null
+  ID_MEDIUM_VINA: number | null
+  ID_BIG_VINA: number | null
+  ID_SMALL_CONCON: number | null
+  ID_MEDIUM_CONCON: number | null
+  ID_BIG_CONCON: number | null
+  ID_SMALL_CONCEPCION: number | null
+  ID_MEDIUM_CONCEPCION: number | null
+  ID_BIG_CONCEPCION: number | null
+  ID_SMALL_RETIROCONDES: number | null
+  ID_MEDIUM_RETIROCONDES: number | null
+  ID_BIG_RETIROCONDES: number | null
+  ID_SMALL_FORCES: number | null
+  ID_MEDIUM_FORCES: number | null
+  ID_BIG_FORCES: number | null
+  ID_SMALL_BUENAVENTURA: number | null
+  ID_MEDIUM_BUENAVENTURA: number | null
+  ID_BIG_BUENAVENTURA: number | null
+  ID_SMALL_URBANO: number | null
+  ID_MEDIUM_URBANO: number | null
+  ID_BIG_URBANO: number | null
+  ID_SMALL_SURCO: number | null
+  ID_MEDIUM_SURCO: number | null
+  ID_BIG_SURCO: number | null
+  ID_SMALL_MIRAFLORES: number | null
+  ID_MEDIUM_MIRAFLORES: number | null
+  ID_BIG_MIRAFLORES: number | null
+  ID_SMALL_SANMIGUEL: number | null
+  ID_MEDIUM_SANMIGUEL: number | null
+  ID_BIG_SANMIGUEL: number | null
+  ID_SMALL_SANJUAN: number | null
+  ID_MEDIUM_SANJUAN: number | null
+  ID_BIG_SANJUAN: number | null
+}
