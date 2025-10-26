c887174012a30888abfad09569e7c62ad4dce52b
Jesús Udiz <jesus.udiz@bettercommerce.cl>
2025-10-25 23:36:53 -0300

    refactor: eliminar la lógica condicional de transporte en la configuración del logger, simplificando la gestión de registros y enfocándose en la claridad de la configuración.

--- config/logger.ts
+++ config/logger.ts
@@ -1,6 +1,5 @@
 import env from '#start/env'
-import app from '@adonisjs/core/services/app'
-import { defineConfig, targets } from '@adonisjs/core/logger'
+import { defineConfig } from '@adonisjs/core/logger'
 
 const loggerConfig = defineConfig({
   default: 'app',
@@ -14,12 +13,6 @@ const loggerConfig = defineConfig({
       enabled: true,
       name: env.get('APP_NAME', 'microservicio-productos'),
       level: env.get('LOG_LEVEL', 'info'),
-      transport: {
-        targets: targets()
-          .pushIf(!app.inProduction, targets.pretty()) // ✅ Desarrollo: pretty
-          .pushIf(app.inProduction, targets.file({ destination: 0 })) // ✅ Producción: stdout
-          .toArray(),
-      },
     },
   },
 })
